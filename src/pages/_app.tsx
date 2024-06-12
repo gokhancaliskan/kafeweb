8244cde79c36abc1dfa8fbf8adcce173028980cc
gokhancaliskan <caliskancompany@gmail.com>
2024-06-12 15:18:46 +0300

    refactor: Optimize image loading, update styles, and use containers for buttons in MainArea component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import Header from "@/components/Header";
 import "../styles/globals.css";
+import Head from "next/head";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/icons/favicon.ico" />
+        <title>Fairytale</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
